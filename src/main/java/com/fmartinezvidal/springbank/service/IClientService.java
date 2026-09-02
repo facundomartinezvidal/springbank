4a0f48ee4a0992d8fc778552cd4b9ed7dabaef9b
Facundo Martinez Vidal <facumartinezvidal@gmail.com>
2026-09-02 11:30:17 -0300

    Delete example

--- src/main/java/com/fmartinezvidal/springbank/service/IClientService.java
+++
@@ -1,8 +0,0 @@
-package com.fmartinezvidal.springbank.service;
-
-import com.fmartinezvidal.springbank.model.Client;
-
-public interface IClientService {
-    Client create(Client client);
-    Client findById(long id);
-}
